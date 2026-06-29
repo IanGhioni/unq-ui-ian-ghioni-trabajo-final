e98baedac776fcc6fb922e0c7e83b45479238e69
ghioni2002@gmail.com <ghioni2002@gmail.com>
2026-06-29 17:32:39 -0300

    añadida validacion de palabras existentes

--- src/components/InputWords.jsx
+++ src/components/InputWords.jsx
@@ -14,8 +14,9 @@ export const InputWords = ({setTimer, setStarted, words, setWords, lastWord, set
         }
     };
 
-    function validate() {
-        const result = validateWord(word, lastWord, words);
+    async function validate() {
+        setError("Validando...")
+        const result = await validateWord(word, lastWord, words);
         if (result.valid) {
             setStarted(true)
             setTimer(15)
--- src/utils/validateWord.js
+++ src/utils/validateWord.js
@@ -1,4 +1,13 @@
-export function validateWord(word, lastWord, words) {
+import { existsWord } from "../service/apiValidate";
+
+export async function validateWord(word, lastWord, words) {
+    const exists = await existsWord(word) 
+    if (!exists) {
+        return {
+            valid: false,
+            message: "¡ La palabra no existe !"
+        }
+    }
 
     if (lastWord !== '') {
         const lastLetter = lastWord.slice(-1).toLowerCase();
