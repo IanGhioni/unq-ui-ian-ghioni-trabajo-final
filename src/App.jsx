a349d2d226b70cbe47d7148f6e187252d403f2b3
ghioni2002@gmail.com <ghioni2002@gmail.com>
2026-07-13 23:59:00 -0300

    terminada not found page y borrada imagen que al final no use jeje

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router'
 import { MainPage } from './pages/MainPage'
 import { PlayPage } from './pages/PlayPage'
+import { NotFound } from './pages/NotFound'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<MainPage />}/>
       <Route path="/play" element={<PlayPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
